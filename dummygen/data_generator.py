84c341653327381d8fd3b4733f3dcfb7384447eb
kuttamuwa <ucok.umut@gmail.com>
2021-12-06 14:57:30 +0300

    v1

--- dummygen/data_generator.py
+++ dummygen/data_generator.py
@@ -1,84 +1,37 @@
 import ast
-import os
 import random
 import uuid
 from datetime import datetime, timedelta
 
 import geopandas as gpd
 import numpy as np
-import osmnx as ox
 import pandas as pd
 from mimesis import Person
-from shapely.geometry import Point
 
 from config import settings
-from db.connector import db, points_table_name, lines_table_name, if_exists
-from dummygen import export_shp_fiona_schema
 
 DUMMY_SETTINGS = settings.DUMMY
-ox.config(use_cache=True, log_console=True)
-crs = {"init": 'epsg:3857'}  # x y
 
 
-class DummyDataGenerator:
-    bbox = ast.literal_eval(DUMMY_SETTINGS.get('bbox'))
-    address = DUMMY_SETTINGS.get('address')
-    distance_delta = DUMMY_SETTINGS.get('footstep_distance', 50)  # meters
-    avg_speed = 1.4  # pedestrian speed: meter/second
+class DummyDataManipulator:
+    crs = DUMMY_SETTINGS.get('crs')
 
+    # dates
     start_date = pd.to_datetime(DUMMY_SETTINGS.get('start_date', datetime.now()))
     end_date = pd.to_datetime(DUMMY_SETTINGS.get('end_date', datetime.now() + timedelta(hours=10)))
     date_mixing = ast.literal_eval(DUMMY_SETTINGS.get('date_mixing', True))
-    random_choice = ast.literal_eval(DUMMY_SETTINGS.get('constant', True))
+
+    # distances
+    random_distance = ast.literal_eval(DUMMY_SETTINGS.get('constant', True))
+    minimum_distance = ast.literal_eval(DUMMY_SETTINGS.get('minimum_distance'))
+    maximum_distance = ast.literal_eval(DUMMY_SETTINGS.get('maximum_distance'))
+    avg_speed = DUMMY_SETTINGS.get('avg_speed', 1.4)
+
+    # person
     dummy_person = Person(locale='tr')
 
-    def __init__(self):
-        # self.recursive_count = DUMMY_SETTINGS.get('recursive_count', 2)
-        # self.recursive_percent = DUMMY_SETTINGS.get('recursive_percent', 50)
-        self.graph = self.get_graph()
-
-        self.points = None
-        self.lines_gdf = None
-
-    def export_points_shapefile(self):
-        """
-        Export shapefile
-        :return:
-        """
-        _path = os.path.abspath('./output')
-        name = 'noname.shp' if self.address is None else f"{self.address.split(',')[0]}.shp"
-
-        self.points.to_file(f'{_path}/{name}', schema=export_shp_fiona_schema)
-
-    def get_graph(self, network_type='all_private'):
-        """
-       Downloads OSM Data and generate random points as pandas DataFrame
-       :return:
-        """
-
-        if self.bbox:
-            G = ox.graph_from_bbox(*self.bbox, network_type=network_type)
-        elif self.address:
-            G = ox.graph_from_place(self.address, network_type=network_type)
-        else:
-            raise ValueError('bbox veya adres parametresi doldurulmalıdır')
-
-        G = ox.project_graph(G, to_crs=crs['init'])
-        return G
-
-    def init_routing(self):
-        self.graph = ox.speed.add_edge_speeds(self.graph)
-        self.graph = ox.speed.add_edge_travel_times(self.graph)
-
-    @staticmethod
-    def _point_to_df(points: [Point], data=None):
-        geom = gpd.points_from_xy([p.x for p in points], [p.y for p in points], crs=crs)
-        gdf = gpd.GeoDataFrame(data=data, geometry=geom, crs=crs)
-
-        return gdf
-
     @classmethod
-    def get_start_date(cls):
+    def random_date(cls):
         if cls.date_mixing:
             start_date = pd.to_datetime(pd.date_range(cls.start_date, cls.end_date, periods=1)[0])
         else:
@@ -86,116 +39,52 @@ class DummyDataGenerator:
 
         return start_date
 
-    def generate_points_along_line_db(self):
-        raise NotImplementedError
-
-    def get_lines_from_db(self):
-        """
-
-        :return:
-        """
-        pass
-
-    def get_lines_by_osmnx(self):
-        """
-        Get lines from osmnx
-        :return:
-        """
-        lines = ox.graph_to_gdfs(self.graph, nodes=False)
-        lines.drop_duplicates('geometry', inplace=True)
-        lines['length'] = lines['geometry'].length
-        lines = lines[lines['length'] > self.distance_delta]  # minimum value
-
-        return lines
-
-    def generate_points_along_line_osmnx(self):
-        """
-        Generate points along lines and sum up.
-
-        :return:
-        """
-        lines = self.get_lines_by_osmnx()
-
-        points = []
-        liness = []
-
-        adding_seconds = self.distance_delta // self.avg_speed
-        for _, l in lines.iterrows():
-            start_date = self.get_start_date()
-            # todo: distances should be random
-            distances = np.arange(0, l.geometry.length, self.distance_delta)
-            for d in distances:
-                subpoints = l.geometry.interpolate(d)
-                start_date += timedelta(seconds=adding_seconds)
-
-                p = {'geometry': subpoints, 'wayid': l.osmid, 'Timestamp': start_date}
-                points.append(p)
-            liness.append(l)
-
-        gdf = gpd.GeoDataFrame(points, crs=crs['init'])
-        # filter if osmid is list
-        gdf = gdf[gdf['wayid'].apply(lambda x: str(x).isdigit())]
-        gdf.drop_duplicates('geometry', inplace=True)
-        gdf.reset_index(inplace=True)
-        gdf.rename(columns={'index': 'ROWID'}, inplace=True)
-
-        # lines
-        lines_gdf = gpd.GeoDataFrame(lines, crs=crs['init'])
-        lines_gdf.drop(columns=[i for i in lines_gdf.columns if i not in ('length', 'geometry', 'osmid')])
-        self.lines_gdf = gpd.GeoDataFrame(lines, crs=crs['init'])
-
-        self.points = gdf
-
-        return gdf
-
-    def generate_random_points_in_area(self) -> gpd.GeoDataFrame:
-        """
-        Downloads OSM Data and generate random points as pandas DataFrame
-        :return:
-        """
-
-        Gp = ox.project_graph(self.graph)
-        count = DUMMY_SETTINGS.get('sample_count', 1000)
-        random_points = ox.utils_geo.sample_points(ox.get_undirected(Gp), count)
-        # _, lines = ox.graph_to_gdfs(G)  # _ is nodes
-
-        # cleaning
-        # lines.drop(columns=['lanes', 'maxspeed', 'bridge', 'junction'], inplace=True)
-
-        return random_points
-
-    def add_dummy_fields_all(self):
-        """
-        You can add your custom fields
-        :return:
-        """
-        # dummy names
-        _len = len(self.points)
-        # todo: gender parametresi koyalim enum random olsun onu da sütuna bas
-        self.points['PersonID'] = uuid.uuid4()  # todo: test et
-        self.points['First Name'] = [self.dummy_person.first_name() for _ in range(_len)]
-        self.points['Last Name'] = [self.dummy_person.last_name() for _ in range(_len)]
-        self.points['Age'] = [self.dummy_person.age(16, 66) for _ in range(_len)]
-        self.points['Quality'] = [random.randint(0, 5) for _ in range(_len)]
-
     def add_dummy_fields_grouped(self, x):
         # todo: gender parametresi koyalim enum random olsun onu da sütuna bas
         x['First Name'] = self.dummy_person.first_name()
-        self.points['PersonID'] = uuid.uuid4()  # todo: test et
+        x['PersonID'] = uuid.uuid4()  # todo: test et
         x['Last Name'] = self.dummy_person.last_name()
         x['Age'] = self.dummy_person.age(16, 66)
         x['Quality'] = random.randint(0, 5)
 
         return x
 
-    def add_dummy_fields_grouping(self, use='wayid'):
-        grp = self.points.groupby(use)
+    def add_dummy_fields_grouping(self, points: gpd.GeoDataFrame, use='wayid'):
+        grp = points.groupby(use)
         df = grp.apply(self.add_dummy_fields_grouped)
 
-        self.points = df
+        return df
 
-    def export_points(self):
-        self.points.to_postgis(points_table_name, con=db, if_exists=if_exists)
+    def generate_points_along_line(self, lines: gpd.GeoDataFrame):
+        """
+        Downloads OSM data if reload = True
+        Generate points along lines and sum up.
 
-    def export_lines(self):
-        self.lines_gdf.to_postgis(lines_table_name, con=db, if_exists=if_exists)
+        :return:
+        """
+
+        points = []
+
+        for _, l in lines.iterrows():
+            start_date = pd.to_datetime(pd.date_range(self.start_date, self.end_date, periods=1)[0])
+
+            how_many_people = np.random.randint(0, 3)
+            distances = np.random.randint(self.minimum_distance, self.maximum_distance, how_many_people)
+            distances.sort()
+
+            for d in distances:
+                adding_seconds = d // self.avg_speed
+                subpoints = l.geometry.interpolate(d)
+                start_date += timedelta(seconds=adding_seconds)
+
+                p = {'geometry': subpoints, 'wayid': l.osmid, 'Timestamp': start_date}
+                points.append(p)
+
+        # points
+        points_gdf = gpd.GeoDataFrame(points, crs=self.crs)
+        points_gdf = points_gdf[points_gdf['wayid'].apply(lambda x: str(x).isdigit())]
+        points_gdf.drop_duplicates('geometry', inplace=True)
+        points_gdf.reset_index(inplace=True)
+        points_gdf.rename(columns={'index': 'ROWID'}, inplace=True)
+
+        return points
--- dummygen/data_puller.py
+++ dummygen/data_puller.py
@@ -1,4 +1,20 @@
-class DataPuller:
+import ast
+from datetime import timedelta
+
+import geopandas as gpd
+import numpy as np
+import osmnx as ox
+import pandas as pd
+from datetime import datetime
+
+from config import settings
+
+DUMMY_SETTINGS = settings.DUMMY
+
+ox.config(use_cache=True, log_console=True)
+
+
+class DataStore:
     """
     POIleri çekeceğiz -> osmnx
     Lineları çekeceğiz -> osmnx
@@ -7,7 +23,83 @@ class DataPuller:
     POI'ler ile müşterilere intersection, contains, within yapalım
 
     """
+    graph = None
+
+    crs = DUMMY_SETTINGS.get('crs')
+    bbox = ast.literal_eval(DUMMY_SETTINGS.get('bbox'))
+    address = DUMMY_SETTINGS.get('address')
+    reload_data = ast.literal_eval(DUMMY_SETTINGS.get('reload', False))
+    network_type = DUMMY_SETTINGS.get('network_type', 'all_private')
+    routing = ast.literal_eval(DUMMY_SETTINGS.get('routing', False))
+
+    # time params
+    start_date = pd.to_datetime(DUMMY_SETTINGS.get('start_date', datetime.now()))
+    end_date = pd.to_datetime(DUMMY_SETTINGS.get('end_date', datetime.now() + timedelta(hours=10)))
+
+    def __new__(cls):
+        if cls.graph is None:
+            cls.graph = cls.download_graph(network_type=cls.network_type)
+        if cls.routing:
+            cls.init_routing()
 
     def __init__(self):
+        self.graph = self.download_graph()
+
+        self.points = None
+        self.lines = None
+        self.polygons = None
+
+    @classmethod
+    def download_graph(cls, network_type='all_private'):
+        """
+       Downloads OSM Data and generate random points as pandas DataFrame
+       :return:
+        """
+
+        if cls.bbox:
+            G = ox.graph_from_bbox(*cls.bbox, network_type=network_type)
+        elif cls.address:
+            G = ox.graph_from_place(cls.address, network_type=network_type)
+        else:
+            raise ValueError('bbox veya adres parametresi doldurulmalıdır')
+
+        G = ox.project_graph(G, cls.crs)
+        return G
+
+    @classmethod
+    def init_routing(cls):
+        cls.graph = ox.speed.add_edge_speeds(cls.graph)
+        cls.graph = ox.speed.add_edge_travel_times(cls.graph)
+
+    def set_lines_from_db(self):
+        """
+
+        :return:
+        """
         pass
 
+    def set_lines_by_osmnx(self):
+        """
+        Get lines from osmnx
+        :return:
+        """
+        if self.lines is None:
+            lines = ox.graph_to_gdfs(self.graph, nodes=False)
+            lines.drop_duplicates('geometry', inplace=True)
+            lines['length'] = lines['geometry'].length
+            self.lines = lines
+
+            return lines
+
+    def set_random_points_in_area(self) -> gpd.GeoDataFrame:
+        """
+        Downloads OSM Data and generate random points as pandas DataFrame
+        :return:
+        """
+
+        Gp = ox.project_graph(self.graph)
+        count = DUMMY_SETTINGS.get('sample_count', 1000)
+        random_points = ox.utils_geo.sample_points(ox.get_undirected(Gp), count)
+
+        self.points = random_points
+        return random_points
