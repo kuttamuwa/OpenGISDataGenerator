50b5463100d50ef59624a2a3d50c1528e74db995
kuttamuwa <ucok.umut@gmail.com>
2021-12-14 14:05:57 +0300

    CLI is added

--- main.py
+++ main.py
@@ -8,13 +8,42 @@ Purpose: Collects
 @Author: Umut Ucok, 2021
 
 """
+import os.path
+
+import click
+
 from dummygen.data_generator import DummyDataManipulator
 from dummygen.data_puller import DataStore
 
-# agp = argparse.ArgumentParser()
-# agp.add_argument('config', help='settings.toml dosyasını yükle')
 
-if __name__ == '__main__':
+@click.command()
+@click.option('-c', '--config', help='Please inspect README.md and .toml file',
+              prompt='Input your config file or we will use default', type=click.Path(),
+              default="")
+def app(config):
+    click.echo(click.format_filename(config))
+    with open('settings.toml', 'w') as writer:
+        writer.truncate()
+        if config != "":
+            # file check
+            if not os.path.exists(config):
+                raise FileNotFoundError("We cannot access to config !")
+
+            click.echo("We are using custom config !")
+            writer.writelines(click.open_file(config).readlines())
+        else:
+            click.echo("We are using default config !")
+            writer.writelines(click.open_file('defaultsettings.toml').readlines())
+
+        writer.close()
+
+    # app
     dummy = DummyDataManipulator()
     puller = DataStore()
-    print("Finished !")
+
+    print("Finished")
+    return dummy, puller
+
+
+if __name__ == '__main__':
+    dummy, puller = app()
